c4a5356d92e1c5ae152806791501d0b4b8f7aa65
mercy-ndungu <mercyndungukirigo@gmail.com>
2022-07-06 14:51:40 +0300

    implemented fragment navigation.

--- app/src/main/java/com/kirigo/financialtracker/HomeActivity.kt
+++ app/src/main/java/com/kirigo/financialtracker/HomeActivity.kt
@@ -2,10 +2,52 @@ package com.kirigo.financialtracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kirigo.financialtracker.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+//        binding = ActivityHome2Binding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//        setupBottomNav()
+    }
+
+    fun setupBottomNav() {
+        binding.bnvBottom.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.Home -> {
+                    val transaction = supportFragmentManager.beginTransaction()
+                    transaction.replace(R.id.fcvHome, HomeFragment())
+                    transaction.commit()
+                    true
+                }
+                R.id.Wallets -> {
+                    val trasaction = supportFragmentManager.beginTransaction()
+                    trasaction.replace(R.id.fcvHome, WalletFragment())
+                    trasaction.commit()
+                    true
+
+                }
+                R.id.Analytics -> {
+                    val trasaction = supportFragmentManager.beginTransaction()
+                    trasaction.replace(R.id.fcvHome, AnalyticsFragment())
+                    trasaction.commit()
+                    true
+
+                }
+                R.id.Profile -> {
+                    val trasaction = supportFragmentManager.beginTransaction()
+                    trasaction.replace(R.id.fcvHome, ProfileFragment())
+                    trasaction.commit()
+                    true
+
+                }
+                else -> false
+            }
+        }
     }
 }
+
